d9f6add07236b64c190eb87b880e80141b775019
Marcus Näslund <naslundx@gmail.com>
2025-03-18 13:28:08 +0100

    skip pip and hw

--- pi/start.sh
+++ pi/start.sh
@@ -5,6 +5,6 @@ cd ~/pirobot/pi
 source ~/.venv/bin/activate
 git stash
 git pull
-pip install -r requirements.txt
-python3 -m hw_interface.main &
+# pip install -r requirements.txt
+# python3 -m hw_interface.main &
 cd server && fastapi run main.py --host 0.0.0.0 --port 80
